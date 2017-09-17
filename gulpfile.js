0dccf3ea76d41577e7567ff0cd84cde6f9d365b0
Maddison Hellstrom <maddy@thundr.net>
2017-09-17 16:25:38 -0700

    Add gulp-jshint

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
-var gulp   = require('gulp');
-var concat = require('gulp-concat');
-var del    = require('del');
-var os     = require('os');
-
+const gulp   = require('gulp');
+const concat = require('gulp-concat');
+const jshint = require('gulp-jshint');
+const del    = require('del');
+const os     = require('os');
 
 var paths = {
   scripts: ['conf.priv.js', 'conf.js']
@@ -12,7 +12,13 @@ gulp.task('clean', function() {
   return del(['build']);
 });
 
-gulp.task('build', ['clean'], function() {
+gulp.task('lint', function() {
+  return gulp.src(paths.scripts)
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('build', ['clean', 'lint'], function() {
   return gulp.src(paths.scripts)
     .pipe(concat('.surfingkeys'))
     .pipe(gulp.dest('build'));

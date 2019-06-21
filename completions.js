91a80b7d594d0be08dd0f3adc48f60d69ce2fba1
Maddison Hellstrom <maddy@na.ai>
2019-06-21 13:15:39 -0700

    [source:feat] additional DuckDuckGo completions
    - I'm Feeling Lucky
    - Images
    - Videos
    - News
    - Maps

--- completions.js
+++ completions.js
@@ -648,6 +648,52 @@ completions.dd = {
 
 completions.dd.callback = response => JSON.parse(response.text).map(r => r.phrase)
 
+// DuckDuckGo - I'm Feeling Lucky
+completions.D = {
+  alias:  "D",
+  name:   "duckduckgo-lucky",
+  search: "https://duckduckgo.com/?q=\\",
+  compl:  "https://duckduckgo.com/ac/?q=\\",
+  callback: completions.dd.callback,
+}
+
+// DuckDuckGo Images
+completions.di = {
+  alias:  "di",
+  name:   "duckduckgo-images",
+  search: "https://duckduckgo.com/?ia=images&iax=images&q=",
+  compl:  "https://duckduckgo.com/ac/?ia=images&iax=images&q=",
+  callback: completions.dd.callback,
+}
+
+// DuckDuckGo Videos
+completions.dv = {
+  alias:  "dv",
+  name:   "duckduckgo-videos",
+  search: "https://duckduckgo.com/?ia=videos&iax=videos&q=",
+  compl:  "https://duckduckgo.com/ac/?ia=videos&iax=videos&q=",
+  callback: completions.dd.callback,
+}
+
+
+// DuckDuckGo News
+completions.dn = {
+  alias:  "dn",
+  name:   "duckduckgo-news",
+  search: "https://duckduckgo.com/?iar=news&ia=news&q=",
+  compl:  "https://duckduckgo.com/ac/?iar=news&ia=news&q=",
+  callback: completions.dd.callback,
+}
+
+// DuckDuckGo Maps
+completions.dm = {
+  alias:  "dm",
+  name:   "duckduckgo-maps",
+  search: "https://duckduckgo.com/?ia=maps&iax=maps&iaxm=places&q=",
+  compl:  "https://duckduckgo.com/ac/?ia=maps&iax=maps&iaxm=places&q=",
+  callback: completions.dd.callback,
+}
+
 // Google
 completions.go = {
   alias:  "go",

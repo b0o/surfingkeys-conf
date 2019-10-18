1bc2e313eb3d1acc8d1729eac5c0c966c6b0d0e7
Paweł Sola <kudlok@mail.ru>
2019-10-18 21:35:52 +0200

    User own settings injection custom settings template creation

---
+++ conf.cust.example.js
@@ -0,0 +1,42 @@
+/*
+Place for any custom settings/mappings (mostly documented on https://github.com/brookhong/Surfingkeys
+ */
+
+// const { categories } = require("./help")
+// mappings must go in this specific format
+const maps = {
+  /*
+  define mappings in format:
+   "domain.com": [ // or just "general"
+     {
+       alias: "", // here keybinding
+       category: 0, // best from the categories requirement
+       description: "The description of the mapping", // self explanatory
+       // Now either of the:
+       map: "", // the key remapping, of:
+       callback: () => {} // the callback
+     }
+   ]
+   */
+
+}
+// note that every alias key has to be present in the maps keys
+const aliases = {
+  /*
+    "domain.com": [
+      "example.com" // also known as
+    ]
+   */
+}
+// custom settings (for example: theme)
+const conf = {
+  /*
+    theme: 'body: { bg-color: "black" }'
+   */
+}
+
+module.exports = {
+  maps,
+  aliases,
+  conf,
+}

e64dfcc94688c87df8d1fbfd6187c4fa0f71571c
Maddison Hellstrom <maddy@na.ai>
2022-11-15 20:05:28 -0800

    Fix webpack build when importing modules with copyright notices

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+import TerserPlugin from 'terser-webpack-plugin'
+
 export default {
   mode:   "production",
   module: {
@@ -6,6 +8,20 @@ export default {
         test: /\.json$/i,
         type: "asset/source",
       },
+    ],
+  },
+  optimization: {
+    // Disable terser's default behavior of creating a separate surfingkeys.js.LICENSE.txt file
+    // because this trips up our gulpfile
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+        terserOptions: {
+          format: {
+            comments: false,
+          },
+        },
+      }),
     ],
   },
   experiments: {

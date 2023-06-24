0b85cbecdf6fd12d72fc37d0f1885cf034529838
Maddison Hellstrom <maddy@na.ai>
2023-06-23 21:05:46 -0700

    tweak chatgpt actions

--- src/actions.js
+++ src/actions.js
@@ -1221,16 +1221,24 @@ actions.cg = {}
 actions.cg.getNewChatLink = () =>
   [...document.querySelectorAll("a")].find((a) => a.innerText === "New chat")
 
-actions.cg.newChat = () => {
+actions.cg.newChat = async () => {
+  const clickNewChat = async (newChatLink) => {
+    newChatLink.click()
+    const gpt4LinkQuery = () => [...document.querySelectorAll('li[class*="group/toggle"]')].find((li) => li.innerText === "GPT-4")
+    return await util.until(gpt4LinkQuery)
+  }
+
   const a = actions.cg.getNewChatLink()
   if (a) {
-    a.click()
+    const gpt4Link = await clickNewChat(a)
+    gpt4Link.querySelector("button>div").click()
     return
   }
-  location.assign("/chat")
+
+  location.assign("https://chat.openai.com/?model=gpt-4")
 }
 
 actions.cg.getChatLinks = () =>
-  actions.cg.getNewChatLink().nextSibling.querySelectorAll("a")
+  actions.cg.getNewChatLink().parentElement.nextSibling.nextSibling.querySelectorAll("a")
 
 export default actions
